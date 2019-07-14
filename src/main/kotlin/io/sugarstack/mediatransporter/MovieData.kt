33f11c6cb95c9a2f19d45a52596b65df5c360e4c
Steve Coward <steve.coward@gmail.com>
2019-07-13 20:22:41 -0400

    fixes missing init for Movie data class and adds missing gradle files

--- src/main/kotlin/io/sugarstack/mediatransporter/MovieData.kt
+++ src/main/kotlin/io/sugarstack/mediatransporter/MovieData.kt
@@ -2,8 +2,12 @@ package io.sugarstack.mediatransporter
 
 import java.nio.file.Path
 
-data class MovieData(val title: String, val year: String, val resolution: String, val path: Path) {
+data class MovieData(var title: String, val year: String, val resolution: String, val path: Path) {
     lateinit var sharePath: Path
+
+    init {
+        title = Utils.toTitleCase(title.replace(".", " "))
+    }
 
     override fun toString(): String {
         return "MovieData(title='$title', year='$year', resolution='$resolution', path=${path.fileName})"
